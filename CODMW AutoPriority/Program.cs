40789bb505b65c2de1c6875d5532c5088cf87c47
Alessandro Serafini <mralex9801@gmail.com>
2020-07-14 13:08:27 +0200

    first

---
+++ CODMW AutoPriority/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace CODMW_AutoPriority
+{
+    class Program
+    {
+		static async Task Main(string[] args)
+		{
+
+			[DllImport("kernel32.dll")]
+			static extern IntPtr GetConsoleWindow();
+			[DllImport("user32.dll")]
+			static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+			var handle = GetConsoleWindow();
+			ShowWindow(handle, 0);
+			while(true)
+			{
+				Process[] game = Process.GetProcessesByName("ModernWarfare");
+				if (game.Length != 0)
+				{
+					try
+					{
+						game[0].PriorityClass = ProcessPriorityClass.Normal;
+					}
+					catch (Exception e)
+					{
+					}
+				}
+				await Sleep(10000);
+			}
+		}
+		private static async Task Sleep(int seconds)
+		{
+			await Task.Delay(seconds);
+		}
+	}
+}
