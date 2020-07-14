4c98a477b2a9f2810e1c3a744bf56ea0fc0da0fa
Alessandro Serafini <mralex9801@gmail.com>
2020-07-14 13:22:11 +0200

    Fix multiple sessions

--- CODMW AutoPriority/Program.cs
+++ CODMW AutoPriority/Program.cs
@@ -9,24 +9,28 @@ namespace CODMW_AutoPriority
     {
 		static async Task Main(string[] args)
 		{
-
+            if (anotherIstance()) return;
+            
 			[DllImport("kernel32.dll")]
 			static extern IntPtr GetConsoleWindow();
 			[DllImport("user32.dll")]
 			static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 			var handle = GetConsoleWindow();
 			ShowWindow(handle, 0);
+
 			while(true)
 			{
-				Process[] game = Process.GetProcessesByName("ModernWarfare");
-				if (game.Length != 0)
 				{
-					try
+					Process[] game = Process.GetProcessesByName("ModernWarfare");
+					if (game.Length != 0)
 					{
-						game[0].PriorityClass = ProcessPriorityClass.Normal;
-					}
-					catch (Exception e)
-					{
+						try
+						{
+							game[0].PriorityClass = ProcessPriorityClass.Normal;
+						}
+						catch (Exception e)
+						{
+						}
 					}
 				}
 				await Sleep(10000);
@@ -36,5 +40,11 @@ namespace CODMW_AutoPriority
 		{
 			await Task.Delay(seconds);
 		}
+
+		private static Boolean anotherIstance()
+        {
+			Process[] p = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName);
+			return p.Length > 1;
+        }
 	}
 }
